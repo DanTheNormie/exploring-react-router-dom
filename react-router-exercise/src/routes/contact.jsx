8de0fa6503378d19fc0152d1619407df861f04d2
CmdDaniel <cmd.princedaniel@gmail.com>
2023-07-29 17:20:20 +0530

    added edit functionality

--- react-router-exercise/src/routes/contact.jsx
+++ react-router-exercise/src/routes/contact.jsx
@@ -1,14 +1,17 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
 
 export default function Contact() {
-  const contact = {
+  /* const contact = {
     first: "Your",
     last: "Name",
     avatar: "https://placekitten.com/g/200/200",
     twitter: "your_handle",
     notes: "Some notes",
     favorite: true,
-  };
+  }; */
+
+  const {contact} = useLoaderData(); 
 
   return (
     <div id="contact">
@@ -88,3 +91,8 @@ function Favorite({ contact }) {
     </Form>
   );
 }
+
+export async function loader({ params }) {
+    const contact = await getContact(params.contactId);
+    return { contact };
+  }
